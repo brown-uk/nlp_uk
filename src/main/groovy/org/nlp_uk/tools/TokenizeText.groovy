1ba23655b8fe75d2ee7798ef465ac9d124e177cc
Andriy Rysin <arysin@gmail.com>
2019-01-27 15:26:50 -0500

    update LT to 4.4

--- src/main/groovy/org/nlp_uk/tools/TokenizeText.groovy
+++ src/main/groovy/org/nlp_uk/tools/TokenizeText.groovy
@@ -2,7 +2,7 @@
 
 package org.nlp_uk.tools
 
-@Grab(group='org.languagetool', module='language-uk', version='4.3')
+@Grab(group='org.languagetool', module='language-uk', version='4.4')
 @Grab(group='commons-cli', module='commons-cli', version='1.3')
 //@Grab(group='org.codehaus.gpars', module='gpars', version='1.2.1')
 
