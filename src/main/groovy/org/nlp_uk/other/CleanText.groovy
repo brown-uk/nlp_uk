55ddb55e26189f3b2849ee4694621fb92159a56c
Andriy Rysin <arysin@gmail.com>
2019-10-01 12:50:07 -0400

    update LT to 4.7

--- src/main/groovy/org/nlp_uk/other/CleanText.groovy
+++ src/main/groovy/org/nlp_uk/other/CleanText.groovy
@@ -12,7 +12,7 @@
 
 //package org.nlp_uk.other
 
-@Grab(group='org.languagetool', module='language-uk', version='4.5')
+@Grab(group='org.languagetool', module='language-uk', version='4.7')
 @Grab(group='commons-cli', module='commons-cli', version='1.4')
 @Grab(group='ch.qos.logback', module='logback-classic', version='1.2.3')
 
