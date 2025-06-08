43e9c37321d3ef61ea19b51c01262643ac18c76e
Andriy Rysin <arysin@gmail.com>
2025-06-08 09:42:24 -0400

    LT version update
    groovy version update

--- src/main/groovy/ua/net/nlp/tools/TokenizeText.groovy
+++ src/main/groovy/ua/net/nlp/tools/TokenizeText.groovy
@@ -4,8 +4,8 @@ package ua.net.nlp.tools
 
 @GrabConfig(systemClassLoader=true)
 @GrabResolver(name="ltSnapshots", root="https://repo.languagetool.org/artifactory/languagetool-os-snapshot/")
-@Grab(group='org.languagetool', module='languagetool-core', version='6.6-SNAPSHOT')
-@Grab(group='org.languagetool', module='language-uk', version='6.6-SNAPSHOT')
+@Grab(group='org.languagetool', module='languagetool-core', version='6.7-SNAPSHOT')
+@Grab(group='org.languagetool', module='language-uk', version='6.7-SNAPSHOT')
 @Grab(group='ch.qos.logback', module='logback-classic', version='1.4.+')
 @Grab(group='info.picocli', module='picocli', version='4.6.+')
 
